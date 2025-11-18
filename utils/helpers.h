530b76f4565a8f78b66beb31559a6f7497ec971f
Fredrik Etsare <Fredrik@etsare.se>
2025-11-18 18:17:50 +0100

    split with string delimiter added to helper functions

--- utils/helpers.h
+++ utils/helpers.h
@@ -17,6 +17,21 @@ inline std::vector<std::string> split(const std::string& str, char delimiter) {
     return tokens;
 }
 
+inline std::vector<std::string> split(const std::string& str, const std::string& delimiter) {
+    std::vector<std::string> tokens;
+    size_t start = 0;
+    size_t end = str.find(delimiter);
+    
+    while (end != std::string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+        start = end + delimiter.length();
+        end = str.find(delimiter, start);
+    }
+    tokens.push_back(str.substr(start));
+    
+    return tokens;
+}
+
 inline std::string trim(const std::string& str) {
     size_t first = str.find_first_not_of(" \t\n\r");
     if (first == std::string::npos) return "";
