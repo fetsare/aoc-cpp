0f3d91437a8857a45ae867f94ec7f92576ed6e49
Fredrik Etsare <Fredrik@etsare.se>
2025-11-18 18:15:16 +0100

    day1:

--- utils/helpers.h
+++ utils/helpers.h
@@ -17,6 +17,21 @@ inline std::vector<std::string> split(const std::string& str, char delimiter) {
     return tokens;
 }
 
+inline std::vector<std::string> split(const std::string& str, const std::string& delimiter) {
+    std::vector<std::string> tokens;
+    size_t start = 0;
+    size_t end = str.find(delimiter);
+    
+    while (end != std::string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+        start = end + delimiter.length();
+        end = str.find(delimiter, start);
+    }
+    tokens.push_back(str.substr(start));
+    
+    return tokens;
+}
+
 inline std::string trim(const std::string& str) {
     size_t first = str.find_first_not_of(" \t\n\r");
     if (first == std::string::npos) return "";
@@ -32,17 +47,18 @@ inline long long toLong(const std::string& str) {
     return std::stoll(str);
 }
 
-inline std::string readInput(const std::string& filename) {
+inline std::vector<std::string> readLines(const std::string& filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Error: Could not open file " << filename << std::endl;
         exit(1);
     }
-    std::string content, line;
+    std::vector<std::string> lines;
+    std::string line;
     while (std::getline(file, line)) {
-        content += line + "\n";
+        lines.push_back(line);
     }
-    return content;
+    return lines;
 }
 
 #endif //HELPERS_H
